b5150aa6ce88cc9ebfb9c6a8fb868029fc683e81
Vincent Hanquez <vincent@snarc.org>
2016-11-22 13:17:17 +0000

    add further bindings to BIGNUM functions

--- Crypto/OpenSSL/BN/Foreign.hs
+++ Crypto/OpenSSL/BN/Foreign.hs
@@ -28,7 +28,67 @@ foreign import ccall unsafe "BN_bn2bin"
 foreign import ccall unsafe "BN_bin2bn"
     ssl_bn_bin2 :: Ptr CUChar -> CInt -> Ptr BIGNUM -> IO (Ptr BIGNUM)
 
--- bn_num_bytes is a macro, 
+-- setter
+
+foreign import ccall unsafe "BN_set_word"
+    ssl_bn_set_word :: Ptr BIGNUM -> CULong -> IO CInt
+
+ssl_bn_zero :: Ptr BIGNUM -> IO CInt
+ssl_bn_zero p = ssl_bn_set_word p 0
+
+ssl_bn_one :: Ptr BIGNUM -> IO CInt
+ssl_bn_one p = ssl_bn_set_word p 1
+
+-- arithmetic operations
+
+foreign import ccall unsafe "BN_add" -- r = a + b
+    ssl_bn_add :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> IO CInt
+
+foreign import ccall unsafe "BN_sub" -- r = a - b
+    ssl_bn_sub :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> IO CInt
+
+foreign import ccall unsafe "BN_mul" -- r = a * b
+    ssl_bn_mul :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_sqr" -- r = sqrt(a)
+    ssl_bn_sqr :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_div" -- div,rem = a / b
+    ssl_bn_div :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+--foreign import ccall unsafe "BN_mod" -- r = a % b
+--    ssl_bn_mod :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_nnmod" -- r = a % b
+    ssl_bn_nnmod :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+-- arithmetic modular operations
+
+foreign import ccall unsafe "BN_mod_add" -- r = a + b [m]
+    ssl_bn_mod_add :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_mod_sub" -- r = a - b [m]
+    ssl_bn_mod_sub :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_mod_mul" -- r = a * b [m]
+    ssl_bn_mod_mul :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_mod_sqr" -- r = sqrt a [m]
+    ssl_bn_mod_sqr :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+-- exponantiations
+
+foreign import ccall unsafe "BN_exp" -- r = a^p
+    ssl_bn_exp :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_mod_exp" -- r = a^p [m]
+    ssl_bn_mod_exp :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+foreign import ccall unsafe "BN_gcd" -- r = gcd(a,b)
+    ssl_bn_gcd :: Ptr BIGNUM -> Ptr BIGNUM -> Ptr BIGNUM -> Ptr BN_CTX -> IO CInt
+
+
+-- bn_num_bytes is a macro,
 ssl_bn_num_bytes :: Ptr BIGNUM -> IO CInt
 ssl_bn_num_bytes ptr = do
     bits <- ssl_bn_num_bits ptr
